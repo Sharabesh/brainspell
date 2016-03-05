5477d9c74426a504e8cc889086b36ad04d1a3703
Dav Clark <davclark@berkeley.edu>
2016-03-04 17:13:02 -0800

    Further simplifying that pesky stream URL cc @stefanav

--- site/404.php
+++ site/404.php
@@ -52,10 +52,8 @@
 		$getdata = http_build_query(array("query" => $q,'action' => 'article_list'));
 		$opts = array('http' =>array('method'=>'POST','header'=>'Content-type: application/x-www-form-urlencoded'));
 		$context  = stream_context_create($opts);
-		$result = file_get_contents('http://'.$_SERVER['HTTP_HOST'].'/php/brainspell.php?'.$getdata, false, $context);	
-		echo $result;
 		*/
-		$result = file_get_contents('http://'.$_SERVER['SERVER_NAME'].'/php/brainspell.php?'.$getdata, false, $context);
+        $result = file_get_contents('http://localhost/php/brainspell.php?'.$getdata, false, $context);
 		echo $result;	
 	}
 	else
